f53ae9db5a3549ae318546a6095b9e8fbbe0813f
Josue Ruiz <jrc2899@gmail.com>
2023-10-22 19:06:26 -0600

    Update gitignore

---
+++ build/precache-manifest.dc80130abc08dcdabebc8297ce2528e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600e0d385e2006ba32043f9983c212e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb06f1d458824165f05",
+    "url": "/static/css/main.70a48ae7.chunk.css"
+  },
+  {
+    "revision": "45d830b6c8524c2ecaf5",
+    "url": "/static/js/2.5c7177a6.chunk.js"
+  },
+  {
+    "revision": "74756de1ce4fdd488d68",
+    "url": "/static/js/3.f54a1483.chunk.js"
+  },
+  {
+    "revision": "acb06f1d458824165f05",
+    "url": "/static/js/main.e742f86d.chunk.js"
+  },
+  {
+    "revision": "2d2be85258a7f2628cb8",
+    "url": "/static/js/runtime~main.d1566087.js"
+  },
+  {
+    "revision": "dbfb607cfccf6d0a4793a0b3b4fe330e",
+    "url": "/static/media/exit.dbfb607c.svg"
+  },
+  {
+    "revision": "75f5a9186dca1c0f887fc791a52a1813",
+    "url": "/static/media/message.75f5a918.svg"
+  }
+]);
